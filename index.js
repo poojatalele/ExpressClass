89dcfca3b557da8c05ea73072a08c4e65383c9ff
ProductServieSST <Pooja.23BCS10151@ms.sst.scaler.com>
2024-07-03 10:42:17 +0530

    Added post, put, delete

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 let courses = [
     {id: 1, name: "java"},
     {id: 2, name: "javascript"},
@@ -9,6 +11,29 @@ let courses = [
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+});
+
+app.post('/courses', (req, res) => {
+    console.log(req.body);
+    let singleCourse = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(singleCourse);
+    res.send(courses);
+})
+
+app.put('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) 
+        return res.status(404).send('The course with the given ID was not found.');
+    course.name = req.body.name; res.json(course);
+}); 
+
+app.delete('/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+    const index = courses.indexOf(course); courses.splice(index, 1); res.json(course);
 });
 
 app.listen(3000, () => {
